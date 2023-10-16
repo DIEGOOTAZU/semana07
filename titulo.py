e8f9b18cf593f459ef79d34c684f28f918340ad0
diego otau <diego@gmail.com>
2023-10-16 11:12:29 -0500

    listo

---
+++ titulo.py
@@ -0,0 +1,25 @@
+# Importamos la libreria
+import camelcase
+
+nombre = "diego armando otau quispe"
+
+print(nombre.upper())
+print(nombre.title())
+
+
+#Creamos un objeto llamado cam
+cam = camelcase.CamelCase()
+print("Con camelcase....")
+
+# imprimimos el nombre en formato título
+# utilizamos camelcase
+print(cam.hump(nombre))
+
+# Para resolver el problema
+# creamos otro objeto llamado cam2
+# al definir el objeto, incluimos los argumentos
+# 'flor' y 'león' 
+cam2 = camelcase.CamelCase('diego','quispe')
+print(cam2.hump(nombre))
+
+
